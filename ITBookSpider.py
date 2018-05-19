107013f8f6351e5881f98c07836927e1894cae77
xuluHu <huwenjiedeyouxiang@126.com>
2018-05-19 12:52:49 +0800

    添加写入功能读取数据

--- ITBookSpider.py
+++ ITBookSpider.py
@@ -4,10 +4,10 @@ import time
 import re
 from bs4 import BeautifulSoup as Bs
 
-url = "https://book.douban.com/tag/编程?start=0&type=S"
+url = "https://book.douban.com/tag/编程?start={position}&type=S"
 kv = {'user-agent': 'Mozilla/5.0'}
 proxies = { "http": "http://39.71.43.214:8118", "https": "http://42.51.12.2:808" }
-
+tableTitle = ["title", "author", "publish", "secondTitle", "originName", "translator", "publishYear", "Pages", "price", "ISBN"]
 def getHtml(url):
     try:
         r = requests.get(url, headers=kv)
@@ -25,51 +25,60 @@ def getItemUrls(resText):
     for tag in tags:
         if ('title' in tag.attrs.keys()):
             itemUrls.append(tag.attrs['href'])
-            # time.sleep(1)
     return itemUrls
+
+def dealReExpress(rematch, dict, index):
+    if(rematch):
+        dict[tableTitle[index]] = rematch.group(1).replace('\n', "")
+        return rematch.group(1).replace('\n', "")
+    else:
+        dict[tableTitle[index]] = "无"
+        return ""
+
 
 #使用正则表达式获得内容
 def dealItemUrls(itemUrls):
+    contentArray = []
     for item in itemUrls:
+        contentDict = {}
         time.sleep(2)
         resText = getHtml(item)
         soup = Bs(resText, 'lxml')
-        tag = soup.find(id="info")
-        text = tag.text.replace(" ", "")
-        #print(text)
-        author = re.search('作者:\s*(.*?)出版社', text, re.S).group(1).replace('\n', "")
-        publish = re.search('出版社:\s*(.*?)\s', text, re.S).group(1).replace('\n', "")
-
-        secondTitle = re.search('原作名:?(.*)\s', text, re.S)
-        if secondTitle:
-            secondTitle = secondTitle.group(1)
-        else:
-            secondTitle = ""
-
-        originName = re.search('原作名:?(.*)\s', text, re.S)
-        if originName:
-            originName = originName.group(1)
-        else:
-            originName = ""
-
-
-        translator = re.search('译者:\s*(.*?)\s', text, re.S).group(1).replace('\n', "")
-        publishYear = re.search('出版年:\s*(.*?)\s', text, re.S).group(1).replace('\n', "")
-        pages = re.search('页数:\s*(.*?)\s', text, re.S).group(1).replace('\n', "")
-        price = re.search('定价:\s*(.*?)\s', text, re.S).group(1).replace('\n', "")
-        isbn = re.search('ISBN:\s*(.*)', text, re.S).group(1).replace('\n', "")
-        print(author,"\n", publish, "\n",translator,"\n",publishYear,"\n",pages, "\n",price,"\n", isbn)
+        tagTitle = soup.find("span", {"property": "v:itemreviewed"})
+        title  = tagTitle.text
+        contentDict[tableTitle[0]] = title
+        tagBody = soup.find(id="info")
+        text = tagBody.text.replace(" ", "")
+        author = dealReExpress(re.search('作者:?\s*(.*?)出版社', text, re.S), contentDict, 1)
+        publish = dealReExpress(re.search('出版社:?\s*(.*?)\s', text, re.S), contentDict, 2)
+        secondTitle = dealReExpress(re.search('副标题:?\s*(.*?)\s', text, re.S), contentDict, 3)
+        originName = dealReExpress(re.search('原作名:?\s*(.*?)\s', text, re.S), contentDict, 4)
+        translator = dealReExpress(re.search('译者:?\s*(.*?)\s', text, re.S), contentDict, 5)
+        publishYear = dealReExpress(re.search('出版年:?\s*(.*?)\s', text, re.S), contentDict, 6)
+        pages = dealReExpress(re.search('页数:?\s*(.*?)\s', text, re.S), contentDict, 7)
+        price = dealReExpress(re.search('定价:?\s*(.*?)\s', text, re.S), contentDict, 8)
+        isbn = dealReExpress(re.search('ISBN:?\s*(.*)', text, re.S), contentDict, 9)
+        print(title,"\n", author,"\n", publish, "\n",secondTitle,"\n",originName,"\n", translator,"\n",publishYear,"\n",pages, "\n",price,"\n", isbn)
         print("---------------------------------")
-
-items = getItemUrls(getHtml(url))
-dealItemUrls(items)
-
+        contentArray.append(contentDict)
+    return contentArray
 
 # 写入csv保存
-row = []
-row.append("xiaohu")
-row.append("12")
-out = open("test.csv", "a", newline="")
-csv_writer = csv.writer(out, dialect="excel")
-csv_writer.writerow(row)
-out.close()
+def writeIntoCsv(row):
+    csv_writer = csv.writer(out, dialect="excel")
+    csv_writer.writerow(row)
+
+if __name__ =='__main__':
+    out = open("ITBook.csv", "a", newline="")
+    writeIntoCsv(tableTitle)
+    for i in range(0, 980, 20):
+        items = getItemUrls(getHtml(url.format(position=i)))
+        contentArray = dealItemUrls(items)
+        for item in contentArray:
+            row = []
+            for(k,v) in item.items():
+                row.append(v)
+            writeIntoCsv(row)
+
+    out.close()
+
